dde09f769c217824b33f5ce5e9a1f1a0cde83c49
Shaquon <shaquonkelley@gmail.com>
2020-09-28 23:43:01 -0400

    refactored getAllPhotos endpoint

--- server/index.js
+++ server/index.js
@@ -7,9 +7,29 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 
+const getAllPhotos = function(callback) {
+  // need to query the database for all users
+  // this function will be called in a controller triggered by a GET request
+  // need to find how to do it without sqlLite
+  console.log(model.photo)
+  model.photo.find({}, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data);
+    }
+  });
+};
 
 app.get('/', (req, res) => {
   res.status(201).send('success!');
+});
+
+app.get('/photos', (req, res) => {
+  // cosole.log(model.videogame.find)
+  getAllPhotos((data) => {
+    res.status(200).send(data);
+  });
 });
 
 app.listen(3003, () => {
