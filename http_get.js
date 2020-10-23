cd91a9248a310e3b356fa03d32a4f6e80ba5d3dd
JessicaWildermuth <jwildermuth536@gmail.com>
2020-10-23 13:10:44 -0700

    Add random resource id to k6 stress test

--- http_get.js
+++ http_get.js
@@ -1,20 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { getRandomNumber } from './dataGenerators/helperFunctions.js'
+
 export let options = {
   stages: [
-    { duration: '1m', target: 200 }, // below normal load
-    { duration: '1m', target: 300 },
-    { duration: '1m', target: 400 },
-    { duration: '1m', target: 500 },
-    { duration: '1m', target: 600 },
-    { duration: '1m', target: 700 },
-    // { duration: '2m', target: 200 }, // normal load
-    // { duration: '5m', target: 200 },
-    // { duration: '2m', target: 300 }, // around the breaking point
-    // { duration: '5m', target: 300 },
-    // { duration: '2m', target: 400 }, // beyond the breaking point
-    // { duration: '5m', target: 400 },
-    // { duration: '10m', target: 0 }, // scale down. Recovery stage.
+    { duration: '30s', target: 200 }, // below normal load
+    { duration: '30s', target: 300 },
+    { duration: '30s', target: 400 },
+    { duration: '30s', target: 500 },
+    { duration: '30s', target: 600 },
+    { duration: '30s', target: 700 },
   ],
 };
 export default function () {
@@ -22,28 +17,10 @@ export default function () {
   let responses = http.batch([
     [
       'GET',
-      `${BASE_URL}/restaurants/1/photos`,
+      `${BASE_URL}/restaurants/${getRandomNumber(30000000, 40000000)}/photos`,
       null,
-      // { tags: { name: 'PublicCrocs' } },
     ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/2/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/3/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/4/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
   ]);
   sleep(1);
 }
+// ${getRandomNumber(30000000, 40000000)}
