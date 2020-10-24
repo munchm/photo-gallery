bacc531c23871214f21aa6697f8b8ac2c41a54b9
JessicaWildermuth <jwildermuth536@gmail.com>
2020-10-24 11:49:00 -0700

    Create node cluster to improve rps

--- http_get.js
+++ http_get.js
@@ -1,26 +1,38 @@
-import http from 'k6/http';
+// import http from 'k6/http';
 import { sleep } from 'k6';
 import { getRandomNumber } from './dataGenerators/helperFunctions.js'
 
+
+// export default function () {
+
+//   let responses = http.get(`http://localhost:8080/restaurants/${getRandomNumber(1, 10000000)}/photos`);
+//   sleep(.1);
+// }
+// // ${getRandomNumber(30000000, 40000000)}
+
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export let errorRate = new Rate('errors');
+
 export let options = {
   stages: [
-    { duration: '30s', target: 1000 }, // below normal load
-    { duration: '30s', target: 1100 },
-    { duration: '30s', target: 1200 },
-    { duration: '30s', target: 1300 },
-    { duration: '30s', target: 1400 },
-    { duration: '30s', target: 1500 },
+    { duration: "30s", target: 1000 },
+    { duration: "30s", target: 1100 },
+    { duration: "30s", target: 1200 },
+    { duration: "30s", target: 1300 },
+    { duration: "30s", target: 1400 },
+    { duration: "30s", target: 1500 },
+
   ],
 };
-export default function () {
-  const BASE_URL = 'http://localhost:8080'; // make sure this is not production
-  let responses = http.batch([
-    [
-      'GET',
-      `${BASE_URL}/restaurants/${getRandomNumber(1, 10000000)}/photos`,
-      null,
-    ],
-  ]);
-  sleep(1);
+
+export default function() {
+  var url = `http://localhost:8080/restaurants/${getRandomNumber(1, 10000000)}/photos`;
+
+  check(http.get(url), {
+    'status is 200': r => r.status == 200
+  }) || errorRate.add(1);
+  // sleep(0.5);
 }
-// ${getRandomNumber(30000000, 40000000)}
--- server/index.js
+++ server/index.js
@@ -5,47 +5,60 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const db = require('../db/index.js');
 const path = require('path');
-const morgan = require('morgan');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 
-const app = express();
-const port = 8080;
 
-app.use(morgan('short'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, '/../public')));
-
-
-app.get('/restaurants/:id', (req, res) => {
-  let restaurantId = req.params.id;
-  db.getRestaurantInfo(restaurantId, (error, restaurantInfo) => {
-    if (error) {
-      res.status(404).send(error);
-    } else {
-      res.status(200).send(restaurantInfo);
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        // Create a worker
+        cluster.fork();
     }
-  });
+} else {
+
+    const app = express();
+    const port = 8080;
+
+    app.use(express.json());
+
+    app.use(express.static(path.join(__dirname, '/../public')));
+    app.get('/restaurants/:id', (req, res) => {
+      let restaurantId = req.params.id;
+      db.getRestaurantInfo(restaurantId, (error, restaurantInfo) => {
+        if (error) {
+          res.status(404).send(error);
+        } else {
+          res.status(200).send(restaurantInfo);
+        }
+      });
+    });
+
+    app.get('/restaurants/:id/photos', (req, res) => {
+      let restaurantId = req.params.id;
+      db.getAllPhotosforRestaurant(restaurantId, (error, photos) => {
+        if (error) {
+          res.status(402).send(error);
+        } else {
+          res.status(200).send(photos);
+        }
+      });
+    });
+
+
+    // app.get('/*', (req, res) => {
+    //   res.sendFile('/Users/jwildermuth/hackreactor/SDC/photo-gallery/public/index.html');
+    // })
+
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`);
+    })
+}
+
+
+cluster.on('exit', function(worker, code, signal) {
+  console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
+  cluster.fork();
 });
 
-app.get('/restaurants/:id/photos', (req, res) => {
-  let restaurantId = req.params.id;
-  db.getAllPhotosforRestaurant(restaurantId, (error, photos) => {
-    if (error) {
-      res.status(402).send(error);
-    } else {
-      res.status(200).send(photos);
-    }
-  });
-});
 
-
-app.get('/*', (req, res) => {
-  res.sendFile('/Users/jwildermuth/hackreactor/SDC/photo-gallery/public/index.html');
-})
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-})
